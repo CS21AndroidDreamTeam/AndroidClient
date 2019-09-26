6493e09b01e961b5c0d0eefc88740f84bb9139fc
Patrick Martin <patrickmartin88@gmail.com>
2019-09-26 14:20:21 -0700

    BG and NPC image will change on usage

--- app/src/main/java/com/historyquestwaifuedition/fragments/DialogueInteractionFragment.kt
+++ app/src/main/java/com/historyquestwaifuedition/fragments/DialogueInteractionFragment.kt
@@ -10,10 +10,8 @@ import android.view.ViewGroup
 import com.historyquestwaifuedition.R
 import com.historyquestwaifuedition.api.HistoryQuestApi
 import com.historyquestwaifuedition.api.HistoryQuestApiService
-import com.historyquestwaifuedition.models.Landmark
-import com.historyquestwaifuedition.models.NodeData
-import com.historyquestwaifuedition.models.NonPlayable
-import com.historyquestwaifuedition.models.Player
+import com.historyquestwaifuedition.models.*
+import kotlinx.android.synthetic.main.fragment_dialogue_interaction.*
 
 private const val ARG_PLAYER = "player"
 private const val ARG_LANDMARK = "landmark"
@@ -22,7 +20,8 @@ class DialogueInteraction : Fragment() {
     private lateinit var historyQuestApiService: HistoryQuestApiService
     private lateinit var player: Player
     private lateinit var landmark: NodeData
-    private lateinit var dialoguer: NonPlayable
+    private lateinit var dialogue: NonPlayable
+    var giveItem: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +33,8 @@ class DialogueInteraction : Fragment() {
         historyQuestApiService = HistoryQuestApi(context!!).service
         //Get NPC to Dialogue
 
-
+        val name = landmark.name as String
+        dialogue = NonPlayableFactory.createNonPlayable(name)
 
         //Get item for NPC
     }
@@ -46,10 +46,74 @@ class DialogueInteraction : Fragment() {
         return inflater.inflate(R.layout.fragment_dialogue_interaction, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        when(landmark.name) {
+            "Prehistoric Cave" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgcave)
+                iv_dialogue_npc.setImageResource(R.drawable.npccave)
+            }
+            "Medieval Wizards Tower" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgwizard)
+                iv_dialogue_npc.setImageResource(R.drawable.npcwizard)
+            }
+            "Abandoned Cottage" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgcottage)
+                iv_dialogue_npc.setImageResource(R.drawable.npccottage)
+            }
+            "Alchemists Laboratory" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgalchemist)
+                iv_dialogue_npc.setImageResource(R.drawable.npcalchemist)
+            }
+            "Viking Barracks" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgviking)
+                iv_dialogue_npc.setImageResource(R.drawable.npcviking)
+            }
+            "Roman Colosseum" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgroman)
+                iv_dialogue_npc.setImageResource(R.drawable.npcrome)
+            }
+            "London Shop" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bglondon)
+                iv_dialogue_npc.setImageResource(R.drawable.npclondon)
+            }
+            "Futuristic Lab" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgfuture)
+                iv_dialogue_npc.setImageResource(R.drawable.npclab)
+            }
+            "Barbaric Outpost" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgbarb)
+                iv_dialogue_npc.setImageResource(R.drawable.npcbarbarian)
+            }
+            "Western Town" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgwestern)
+                iv_dialogue_npc.setImageResource(R.drawable.npcwestern)
+            }
+            "Colonial Puritan Church" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgchurch)
+                iv_dialogue_npc.setImageResource(R.drawable.npcpuritan)
+            }
+            "Nazi Meeting Hall" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgmeetinghall)
+                iv_dialogue_npc.setImageResource(R.drawable.npcmeeting)
+            }
+            "Cold War Nuclear Site" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgnuclear)
+                iv_dialogue_npc.setImageResource(R.drawable.npcnuclear)
+            }
+            "Chinese Pagoda" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgpagoda)
+                iv_dialogue_npc.setImageResource(R.drawable.npcpagoda)
+            }
+            "Beached Pirate Ship" -> {
+                iv_dialogue_background.setImageResource(R.drawable.bgpirate)
+                iv_dialogue_npc.setImageResource(R.drawable.npcpirate)
+            }
+            else -> throw IllegalArgumentException("This shouldn't be possible")
 
-
-
+        }
+    }
 
     companion object {
 
